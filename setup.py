c958d4ed823c437f8b10475ce4b60fe03a349407
kazuki <k-asayama@outlook.coml>
2016-04-17 01:09:22 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(name='progressbar',
+	version='0.0.0',
+	author='wassen',
+	author_email='k-asayama@outlook.com',
+	#packages=find_packages(),
+	#scripts=[''],
+	#test_suite = ,
+	#package_dir{'':''}
+	py_modules=['progressbar'] ,
+)
